b6cd497afc481c833d9975c7f1367b02b8c384e3
jshwaa <43734859+jshwaa@users.noreply.github.com>
2019-03-03 15:19:17 -0800

    Create sanger_convert.sh

---
+++ Alignment/DNAseq_SangerConvertandAlign/sanger_convert.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#$ -N DNA_sanger_convert
+#$ -q epyc,pub64
+#$ -pe openmp 24
+#$ -R y
+#$ -t 1-24
+
+module load bwa/0.7.8
+module load samtools/1.3
+module load picard-tools/1.87
+module load java/1.7
+
+cd DNAseq/labeled_sanger
+
+prefix=`head -n $SGE_TASK_ID convertfiles.txt | tail -n 1`
+
+/pub/jcrapser/seqtk/seqtk seq -Q64 -V $prefix | gzip -c > $prefix.sanger.fq.gz
