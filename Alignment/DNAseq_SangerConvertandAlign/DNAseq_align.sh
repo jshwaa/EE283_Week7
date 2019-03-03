b396f1e4a3e77d5963220d610ef94e039595f580
jshwaa <43734859+jshwaa@users.noreply.github.com>
2019-03-03 15:51:32 -0800

    Update DNAseq_align.sh

--- Alignment/DNAseq_SangerConvertandAlign/DNAseq_align.sh
+++ Alignment/DNAseq_SangerConvertandAlign/DNAseq_align.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #$ -N DNAseq_align
-#$ -q bio,pub64
+#$ -q epyc,pub64
 #$ -pe openmp 8
 #$ -R y
 #$ -t 1-12
@@ -20,7 +20,7 @@ dict="../../ref/dmel-all-chromosome-r6.13.fasta.dict"
 prefix=`head -n $SGE_TASK_ID DNAseq.prefixes.txt | tail -n 1`
 
 
-bwa mem -t 8 -M ${ref} ${prefix}_1.fq.gz ${prefix}_2.fq.gz | samtools view -bS - > ./aligned/${prefix}.bam
+bwa mem -t 8 -M ${ref} ${prefix}_1.fq.gz.sanger.fq.gz ${prefix}_2.fq.gz.sanger.fq.gz | samtools view -bS - > ./aligned/${prefix}.bam
 samtools sort aligned/$prefix.bam -o aligned/$prefix.sort.bam
 java -Xmx20g -jar /data/apps/picard-tools/1.87/AddOrReplaceReadGroups.jar I=aligned/$prefix.sort.bam O=aligned/$prefix.RG.bam SORT_ORDER=coordinate RGPL=sanger RGPU=D109LACXX RGLB=Lib1 RGID=$prefix RGSM=$prefix VALIDATION_STRINGENCY=LENIENT
 samtools index aligned/$prefix.RG.bam
