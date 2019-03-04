99a4d2a27d9143a75c33118551b6b45807e54ca8
jshwaa <43734859+jshwaa@users.noreply.github.com>
2019-03-03 21:04:09 -0800

    Update DNAseq_sanger_align.sh

--- Alignment/DNAseq_SangerConvertandAlign/DNAseq_sanger_align.sh
+++ Alignment/DNAseq_SangerConvertandAlign/DNAseq_sanger_align.sh
@@ -12,9 +12,9 @@ module load java/1.7
 
 cd DNAseq/labeled_DNAseq
 
-ref="../../ref/dmel-all-chromosome-r6.13.fasta"
+ref="../../ref/dmel-all-chromosome-r6.26.fasta"
 
-dict="../../ref/dmel-all-chromosome-r6.13.fasta.dict"
+dict="../../ref/dmel-all-chromosome-r6.26.fasta.dict"
 
 
 prefix=`head -n $SGE_TASK_ID DNAseq.prefixes.txt | tail -n 1`
