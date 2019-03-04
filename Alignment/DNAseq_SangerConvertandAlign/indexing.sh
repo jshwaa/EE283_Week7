ecaa3d0cf158df51842db0f611189ec7052922b2
jshwaa <43734859+jshwaa@users.noreply.github.com>
2019-03-03 21:10:25 -0800

    Create indexing.sh

---
+++ Alignment/DNAseq_SangerConvertandAlign/indexing.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#$ -N index
+#$ -q bio,pub64
+#$ -pe openmp 8
+#$ -R y
+
+module load bowtie2/2.2.7
+module load bwa/0.7.8
+module load samtools/1.3
+module load bcftools/1.3
+module load enthought_python/7.3.2
+module load gatk/2.4-7
+module load picard-tools/1.87
+module load java/1.7
+
+ref="ref/dmel-all-chromosome-r6.26.fasta"
+bwa index $ref 
+samtools faidx $ref  
+java -d64 -Xmx128g -jar /data/apps/picard-tools/1.87/CreateSequenceDictionary.jar R=$ref O=ref/dmel-all-chromosome-r6.26.fasta.dict
+bowtie2-build $ref $ref
